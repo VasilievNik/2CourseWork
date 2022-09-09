f5053502bab0c7d78986a09f54a7a655a840cae2
nikit <nikita.veselyev@gmail.com>
2022-09-09 19:33:28 +0300

    3d commit
    Added tests for QuestionService and ExaminerService, maven and mockito

--- src/main/java/com/example/CourseWork/Examiner/ExaminerServiceImpl.java
+++ src/main/java/com/example/CourseWork/Examiner/ExaminerServiceImpl.java
@@ -1,11 +1,13 @@
 package com.example.CourseWork.Examiner;
 
+import com.example.CourseWork.Exceptions.OutOfBoundsException;
 import com.example.CourseWork.Questions.Question;
 import com.example.CourseWork.Questions.QuestionService;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
 @Service
 public class ExaminerServiceImpl implements ExaminerService{
@@ -19,9 +21,22 @@ public class ExaminerServiceImpl implements ExaminerService{
 
     @Override
     public Collection<Question> getQuestions(int amount) {
+        if(amount>questionList.size()){
+            throw new OutOfBoundsException();
+        }
+
+        HashSet<Question> integerHashSet = new HashSet<>();
+
         for(int i=1; i<=amount; i++){
             Question question1 = questionService.getRandomQuestion();
-            questionList.add(question1);
+
+            if (integerHashSet.contains(question1)){
+                i--;
+            }
+            else{
+                integerHashSet.add(question1);
+                questionList.add(question1);
+            }
         }
         return questionList;
     }
